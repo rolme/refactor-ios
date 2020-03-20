61563b72465ba54e737bcca976f12bd645987934
Samuel Franusic <sfranusic@gmail.com>
2020-03-20 09:53:15 -0700

    Add swiftui view for login

---
+++ refactor-ios/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  refactor-ios
+//
+//  Created by Sammy Franusic on 3/20/20.
+//  Copyright © 2020 Ron Parnaso. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Hello, Roland!")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
