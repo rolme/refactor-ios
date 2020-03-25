8e46ffc2f2a5d34bd7b3c88c1a572471fbff80df
Sammy Franusic <sfranusic@gmail.com>
2020-03-25 13:30:45 -0700

    Add method and UI for logging in

--- refactor-ios/LoginView.swift
+++ refactor-ios/LoginView.swift
@@ -7,10 +7,58 @@
 //
 
 import SwiftUI
+import AWSMobileClient
 
 struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
     var body: some View {
-        Text("Hello, Roland!")
+        VStack {
+            TextField("Email", text: $email)
+                .keyboardType(.emailAddress)
+                .textContentType(.emailAddress)
+                .padding(.leading)
+                .padding(.trailing)
+                .frame(height: 50)
+                .background(Color.white)
+            TextField("Password", text: $password)
+                .padding(.leading)
+                .padding(.trailing)
+                .frame(height: 50)
+                .disableAutocorrection(true)
+                .textContentType(.newPassword)
+            Button(action: {
+                authenticate(username: self.email, password: self.password)
+            }) {
+                Text("Log In")
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(minWidth: 300,
+                           alignment: .center)
+                    .background(Color.blue)
+                    .cornerRadius(CGFloat(5))
+            }
+
+            
+            
+        }
+    }
+}
+
+func authenticate(username: String, password: String) {
+    // Use AWSMobileClient to log in.
+    AWSMobileClient.default().signIn(username: username, password: password) { success, error in
+        if success != nil {
+            // Move to main view
+        }
+
+        if let error = error {
+            if let awsError = error as? AWSMobileClientError {
+                print("Error: \(awsError.localizedDescription)")
+            }
+        }
+
     }
 }
 
